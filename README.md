53fdad99d36e672ac662eb85f5c0c536519685ce
Phung Pham <phungpham.tech@gmail.com>
2018-11-17 12:42:54 -0500

    fixed error and updated readMe

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# movie-search
+# Movies Search
+
+## Description
+This app is allow user to search the information on the movie that they added. First, they need to input the movie name of interest to render movie name button, and then click on the movie name button to get the information on that movie.
+
+Click to use this movie-search app: https://phungdpham.github.io/movie-search/
+
+## Technologies
+** html, css, javascript
+** ajax
+** omdbapi
+**
+
+## Key Learning
+** Learn how to use api using Ajax
+** javascript skill to render elements such as buttons, div that shows information.
--- app.js
+++ app.js
@@ -1,75 +1,78 @@
-//Initial an array of movies aded
-var movies = ["matrix", "avenger"];
 
-//display movie information
-function showMovieInfo() {
-    var movie = $(this).attr("data-name");
-    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=2fbfb06c";
+    //Initial an array of movies aded
+    var movies = ["matrix", "avenger"];
 
-    //creating an AJAX call for the specific movie button being clicked
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        //Creating a div to hold the movie
-        var movieDiv = $("<div class='movie'>");
+    //display movie information
+    function showMovieInfo() {
+        var movie = $(this).attr("data-name");
+        var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=2fbfb06c";
 
-        //Storing the rating data
-        var rating = response.Rated;
-        //Creating an element to have the rating displayed
-        var pOne = $("<p>").text("Rating: " + rating);
-        //Displaying the rating
-        movieDiv.append(pOne);
+        //creating an AJAX call for the specific movie button being clicked
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            //Creating a div to hold the movie
+            var movieDiv = $("<div class='movie'>");
 
-        //Storing the release year
-        var released = response.Released;
-        //Creating an element to hold the relase year
-        var pTwo = $("<p>").text("Released: " + released);
-        //Displaying the release year
-        movieDiv.append(pTwo);
+            //Storing the rating data
+            var rating = response.Rated;
+            //Creating an element to have the rating displayed
+            var pOne = $("<p>").text("Rating: " + rating);
+            //Displaying the rating
+            movieDiv.append(pOne);
 
-        //Storing the plot
-        var plot = response.Plot;
-        //Creating an element to hold the plot
-        var pThree = $("<p>").text("Plot: " + plot);
-        //Appending the plot
-        movieDiv.append(pThree);
+            //Storing the release year
+            var released = response.Released;
+            //Creating an element to hold the relase year
+            var pTwo = $("<p>").text("Released: " + released);
+            //Displaying the release year
+            movieDiv.append(pTwo);
 
-        //Retrieving the URL for the images
-        var imgURL = response.Poster;
-        //Creating an element to hold the image
-        var image = $("<img>").attr("src", imgURL);
+            //Storing the plot
+            var plot = response.Plot;
+            //Creating an element to hold the plot
+            var pThree = $("<p>").text("Plot: " + plot);
+            //Appending the plot
+            movieDiv.append(pThree);
 
-        //Appending the image
-        movieDiv.append(image);
+            //Retrieving the URL for the images
+            var imgURL = response.Poster;
+            //Creating an element to hold the image
+            var image = $("<img>").attr("src", imgURL);
 
-        //Putting the entire movie above the previous movies
-        $("#movies-view").prepend(movieDiv);
-    });
-}
-//function to dipslay movie data
-function renderBtn() {
-    //Deleting the movies prior to adding new movie
-    $("#buttons-view").empty();
-    //Looping through the array of movies
-    for (var i = 0; i < movies.length; i++) {
-        //Generating buttons for each movie in the array
-        var a = $("<button>");
-        a.addClass("movie-btn");
-        a.attr("data-name", movies[i]);
-        a.text(movies[i]);
-        $("#buttons-view").append(a);
+            //Appending the image
+            movieDiv.append(image);
+
+            //Putting the entire movie above the previous movies
+            $("#movies-view").prepend(movieDiv);
+        });
     }
-};
-//Handling the events where a movie button is clicked
-$("#add-movie").on("click", function(event) {
-    event.preventDefault();
-    //Grabing the value of new input and adding it to the array of movie
-    var movie = $("#movie-input").val().trim();
-    movies.push(movie);
-    renderBtn();
-    $("#movie-input").val("");
-});
 
-$(document).on("click", ".movie-btn", showMovieInfo);
-renderBtn();
+    //function to dipslay movie data
+    function renderBtn() {
+        //Deleting the movies prior to adding new movie
+        $("#buttons-view").empty();
+        //Looping through the array of movies
+        for (var i = 0; i < movies.length; i++) {
+            //Generating buttons for each movie in the array
+            var a = $("<button>");
+            a.addClass("movie-btn");
+            a.attr("data-name", movies[i]);
+            a.text(movies[i]);
+            $("#buttons-view").append(a);
+        }
+    };
+    //Handling the events where a movie button is clicked
+    $("#add-movie").on("click", function (event) {
+        event.preventDefault();
+        //Grabing the value of new input and adding it to the array of movie
+        var movie = $("#movie-input").val().trim();
+        movies.push(movie);
+        renderBtn();
+        $("#movie-input").val("");
+    });
+
+    $(document).on("click", ".movie-btn", showMovieInfo);
+    renderBtn();
+
